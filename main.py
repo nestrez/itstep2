a8403cc2d4be0c8183aa1e9273dfb95645fc8b04
nestrez <nestorkorpan@gmail.com>
2023-05-07 04:51:13 -0700

    # my_dict = { #     'Macbook': 'pro', #     'tesla': 'X' # } # print(my_dict) # print(my_dict['Macbook']) # # key = 'honda' # value = 'Cv' # my_dict[key] = value # print(my_dict)
    #my_dict2 = dict(Name = 'John', age = 36, country = 'Norway')
    #print(my_dict2)
    #
    #print(my_dict2.keys())
    #
    #print(my_dict2.values())
    #print(my_dict2.items())
    
    # thisdict = dict(Name = 'maks', age = 20, country = 'Ukraine')
    # for x in thisdict:
    #     print([x])
    # for x in thisdict:
    #     print(thisdict[x])
    # for x, y in thisdict:
    #     print([x,y])
    
    # name = input('Enter name')
    # dict2 = {
    #     'Nick': {'phone': '1111',
    #              'insta': '@123'},
    #     'Anna': {'phone': '2222',
    #              'insta': "@456"},
    #     'Tomas': {'phone': '3333',
    #              'insta': "@789"}
    # }
    #
    #
    #
    # user = dict2[input()][input()]
    # print(user)
    
    #
    # if name = (dict2[]):
    #    print(dict2['Nick'])
    #
    # if name = ['Anna']
    #    print(dict2['Anna'])
    #
    #
    #    print(dict2['Tomas'])
    
    # dict3 = {
    #     'brand': 'nike',
    #     'year': '1964',
    #     'make': 'clothes'
    # }
    #
    #
    # deleted = ['year', 'make']
    #
    # for x in deleted:
    #     dict3.pop(x)
    # print(dict3)
    
    # dict4 = {
    #     'Nick': {'phone': '111'},
    #     'Anna': {'phone': '222'},
    #     'Tomas': {'phone': '333'}
    # }
    #
    # user = dict2[input()][input()]
    # print(user)

---
+++ main.py
@@ -0,0 +1,87 @@
+#ТОКЕН
+
+# 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
+
+#................
+
+
+
+
+
+# my_dict = {
+#     'Macbook': 'pro',
+#     'tesla': 'X'
+# }
+# print(my_dict)
+# print(my_dict['Macbook'])
+#
+# key = 'honda'
+# value = 'Cv'
+# my_dict[key] = value
+# print(my_dict)
+
+#my_dict2 = dict(Name = 'John', age = 36, country = 'Norway')
+#print(my_dict2)
+#
+#print(my_dict2.keys())
+#
+#print(my_dict2.values())
+#print(my_dict2.items())
+
+
+# thisdict = dict(Name = 'maks', age = 20, country = 'Ukraine')
+# for x in thisdict:
+#     print([x])
+# for x in thisdict:
+#     print(thisdict[x])
+# for x, y in thisdict:
+#     print([x,y])
+
+# name = input('Enter name')
+# dict2 = {
+#     'Nick': {'phone': '1111',
+#              'insta': '@123'},
+#     'Anna': {'phone': '2222',
+#              'insta': "@456"},
+#     'Tomas': {'phone': '3333',
+#              'insta': "@789"}
+# }
+#
+#
+#
+# user = dict2[input()][input()]
+# print(user)
+
+#
+# if name = (dict2[]):
+#    print(dict2['Nick'])
+#
+# if name = ['Anna']
+#    print(dict2['Anna'])
+#
+#
+#    print(dict2['Tomas'])
+
+
+# dict3 = {
+#     'brand': 'nike',
+#     'year': '1964',
+#     'make': 'clothes'
+# }
+#
+#
+# deleted = ['year', 'make']
+#
+# for x in deleted:
+#     dict3.pop(x)
+# print(dict3)
+
+
+# dict4 = {
+#     'Nick': {'phone': '111'},
+#     'Anna': {'phone': '222'},
+#     'Tomas': {'phone': '333'}
+# }
+#
+# user = dict2[input()][input()]
+# print(user)
